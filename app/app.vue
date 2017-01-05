001a9109e6dab8e6c1b4051bffccd8dc18a902ee
Sascha Dube <sascha.dube@gmail.com>
2017-01-05 17:49:24 +0100

    add style to navbar for pages

--- app/app.vue
+++ app/app.vue
@@ -2,39 +2,36 @@
 
     <div>
 
-        <nav class="uk-navbar-container tm-navbar-container">
+        <div class="uk-navbar-container tm-navbar-container" uk-sticky="top: 1; media: 960">
 
-            <div class="uk-navbar">
+            <nav class="uk-navbar">
 
                 <div class="uk-navbar-left">
+
                     <a href="./" class="uk-navbar-item uk-logo">
                         <span class="uk-margin-small-right" uk-icon="icon: uikit; ratio: 2"></span> UIkit
                     </a>
+
                 </div>
 
                 <div class="uk-navbar-right">
+
+                    <ul class="uk-navbar-nav uk-visible@m">
+                        <li><a href="./docs/">Documentation</a></li>
+                        <li><a href="./changelog">Changelog</a></li>
+                    </ul>
+
                     <div class="uk-navbar-item uk-visible@m">
-                        <div class="uk-grid uk-flex-middle uk-grid" uk-grid>
-                            <div>
-                                <a class="uk-button uk-button-text" href="./docs/" title="Documentation">Documentation</a>
-                            </div>
-                            <div>
-                                <a class="uk-button uk-button-text" href="./changelog" title="View Changelog">Changelog</a>
-                            </div>
-                            <div>
-                                <a class="uk-button uk-button-default tm-button-default" href="./download" title="Download">
-                                    Download <span class="uk-margin-small-left" uk-icon="icon: download"></span>
-                                </a>
-                            </div>
-                        </div>
+                        <a class="uk-button uk-button-default tm-button-default" href="./download">Download <span class="uk-margin-small-left" uk-icon="icon: download"></span></a>
                     </div>
 
                     <a class="uk-navbar-toggle uk-hidden@m" uk-navbar-toggle-icon href="#offcanvas" uk-toggle></a>
+
                 </div>
 
-            </div>
+            </nav>
 
-        </nav>
+        </div>
 
         <router-view></router-view>
 
