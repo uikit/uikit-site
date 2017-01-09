6ad476f9b129d27df836254f78dbf8574f0389bf
janschoenherr <jan@yootheme.com>
2017-01-09 10:05:48 +0100

    cleanup

--- app/navbar.vue
+++ app/navbar.vue
@@ -7,7 +7,7 @@
                 <div class="uk-navbar-left">
 
                     <router-link class="uk-navbar-item uk-logo" to="/">
-                        <img class="uk-margin-small-right" uk-svg :src="'./images/uikit-logo.svg'"> UIkit
+                        <canvas class="uk-margin-small-right" width="28" height="34" uk-svg :src="base + './images/uikit-logo.svg'"></canvas> UIkit
                     </router-link>
 
                 </div>
@@ -20,7 +20,7 @@
                     </ul>
 
                     <div class="uk-navbar-item uk-visible@m">
-                        <router-link class="uk-button uk-button-default tm-button-default uk-icon" to="/download">Download <span uk-icon="icon: download" width="20" height="20"></span>
+                        <router-link class="uk-button uk-button-default tm-button-default uk-icon" to="/download">Download <canvas uk-icon="icon: download" width="20" height="20"></canvas>
                         </router-link>
                     </div>
 
