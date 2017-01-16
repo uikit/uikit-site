f645c72f90d12699ac027b732fc5924408cc3d7a
janschoenherr <jan@yootheme.com>
2017-01-16 17:47:36 +0100

    cleanup

--- docs/app/page.vue
+++ docs/app/page.vue
@@ -1,8 +1,16 @@
+<template>
+
+    <div>
+        <div class="uk-alert uk-alert-danger" v-if="error">{{ error }}</div>
+        <div ref="container"></div>
+    </div>
+
+</template>
+
 <script>
 
     import $ from 'jquery';
     import { parse, openOnCodepen, sluggify } from './util';
-    import Page from '../../app/page.vue';
 
     const navigation = require('./navigation.json');
 
@@ -10,10 +18,9 @@
 
     export default {
 
-        extends: Page,
-
         data: () => ({
-            extension: 'md'
+            error: null,
+            cache: {}
         }),
 
         mounted() {
@@ -43,32 +50,55 @@
 
         watch: {
 
-            page() {
+            $route: {
 
-                this.$parent.component = components.indexOf(this.$route.params.page) != -1 ? this.$route.params.page : false;
+                handler() {
 
-                this.$nextTick(() => {
+                    var page = this.$route.params.page;
 
-                    $('pre code', this.$refs.container).each((i, block) => hljs.highlightBlock(block));
+                    this.error = null;
 
-                    this.$parent.ids = $('h1,h2', this.$refs.container).filter((i, el) => !$(el).parents('.uk-switcher').length).toArray().reduce((ids, el) => {
+                    this.$parent.page = page;
 
-                        el = $(el);
+                    var defer = $.Deferred();
 
-                        let text = el.text().replace('#', ''), id = sluggify(text);
+                    defer.then(page => {
 
-                        el.attr('id', id);
+                        parse(page, (err, content) => {
 
-                        if (el.is('h2')) {
-                            ids[text] = id;
+                            if (err) {
+                                this.page = null;
+                                this.error = err;
+                            } else {
+                                this.setPage(content);
+                            }
+
+                        });
+
+                    }, () => this.error = 'Failed loading page');
+
+                    if (this.cache[page]) {
+                        defer.resolve(this.cache[page]);
+                        return;
+                    }
+
+                    $.get(`pages/${page}.md`, {nc: Math.random()}).then(content => {
+
+                        if (content.trim().indexOf('<!DOCTYPE html>') === 0) {
+                            content = `<div class="uk-text-center">
+                                <h1>404</h1>
+                                <p class="uk-text-large">Page not found!</p>
+                            </div>`
                         }
 
-                        return ids;
+                        this.cache[page] = content;
+                        defer.resolve(content);
 
-                    }, {});
+                    }, err => reject(err));
 
-                });
+                },
 
+                immediate: true
             }
 
         },
@@ -77,16 +107,35 @@
 
             setPage(page) {
 
-                parse(page, (err, content) => {
+                $(page).appendTo($(this.$refs.container).empty());
 
-                    if (err) {
-                        this.page = null;
-                        this.error = err;
-                    } else {
-                        this.page = content;
+                this.$parent.component = components.indexOf(this.$route.params.page) != -1 ? this.$route.params.page : false;
+
+                $('pre code', this.$refs.container).each((i, block) => hljs.highlightBlock(block));
+
+                this.$parent.ids = $('h1,h2', this.$refs.container).filter((i, el) => !$(el).parents('.uk-switcher').length).toArray().reduce((ids, el) => {
+
+                    el = $(el);
+
+                    let text = el.text().replace('#', ''), id = sluggify(text);
+
+                    el.attr('id', id);
+
+                    if (el.is('h2')) {
+                        ids[text] = id;
                     }
 
-                });
+                    return ids;
+
+                }, {});
+
+                if (location.hash && $(location.hash).length) {
+                    scrollTo(0, $(location.hash).offset().top - 100);
+                } else {
+                    scrollTo(0, 0);
+                }
+
+                UIkit.scroll($(this.$refs.container).find('a[href^="#"]'), {offset: 100});
 
             }
 
