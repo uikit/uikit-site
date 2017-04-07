37cb2467b48c226656a541790e38506eacdc1a4b
Florian <florian@yootheme.com>
2017-04-07 14:54:29 +0200

    fix webpack compile error

--- app/pages/pro.vue
+++ app/pages/pro.vue
@@ -1,17 +1,69 @@
 <template>
 
-    <div class="uk-section-primary tm-section uk-preserve-color">
+    <div>
+            
+        <div class="uk-section-primary tm-section uk-preserve-color">
 
-        <navbar class="uk-navbar-transparent uk-light"></navbar>
+            <navbar class="uk-navbar-transparent uk-light"></navbar>
 
-        <div class="uk-section" uk-height-viewport="expand: true">
+            <div class="uk-section" uk-height-viewport="expand: true">
+                <div class="uk-container">
+
+                    <div uk-grid>
+                        <div>
+
+                            <h1 class="uk-h4">YOOtheme Pro</h1>
+                            <h2 class="uk-h1">The website builder for WordPress &amp; Joomla from the UIkit creators</h2>
+
+                        </div>
+                        <div>
+
+                            <img src="test.jpg">
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="uk-section uk-section-primary">
             <div class="uk-container">
 
-                <div uk-grid>
+                <div class="uk-child-width-expand@m uk-child-width-1-2@s" uk-grid>
                     <div>
 
-                        <h1 class="uk-h4">YOOtheme Pro</h1>
-                        <h2 class="uk-h1">The website builder for WordPress &amp; Joomla from the UIkit creators</h2>
+                        <h3>Elements</h3>
+                        <p>Build pages with ready to use UIkit elements like buttons, cards, accordion and many more.</p>
+
+                    </div>
+                    <div>
+
+                        <h3>Header Layouts</h3>
+                        <p>Modify your sticky navbar to dropdown or configure where items inside the header should be displayed.</p>
+
+                    </div>
+                    <div>
+
+                        <h3>Page Builder</h3>
+                        <p>Create your UIkit pages in next to no time. Our innovative page builder makes it possible to arrange sections.</p>
+
+                    </div>
+                    <div>
+
+                        <h3>Real-Time Preview</h3>
+                        <p>Implement your ideas and see immediately how they look like. Our real-time-preview makes it</p>
+
+                    </div>
+                </div>
+
+                <div class="uk-child-width-expand@m" uk-grid>
+                    <div class="uk-flex-last@m">
+
+                        <h1 class="uk-h4">Style Customizer</h1>
+                        <h2 class="uk-h1">Style Customizer</h2>
+                        <p>Arrange visual appearance of your site, including colors, fonts, margins and other style related properties with a full style customizer for our UIkit CSS framework.</p>
 
                     </div>
                     <div>
@@ -21,189 +73,142 @@
                     </div>
                 </div>
 
+                <h2>Pre-built Styles</h2>
+
+                <div class="uk-child-width-1-3@m uk-child-width-1-2@s" uk-grid>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/copper-hill">
+                            <img src="pro/style-copper-hill.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/sonic">
+                            <img src="pro/style-sonic.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fjord">
+                            <img src="pro/style-fjord.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/joline">
+                            <img src="pro/style-joline.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/max">
+                            <img src="pro/style-max.jpg">
+                        </a>
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/horizon">
+                            <img src="pro/style-horizon.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fuse">
+                            <img src="pro/style-fuse.jpg">
+                        </a>
+
+                    </div>
+                </div>
+
+                <hr>
+
+                <h1 class="uk-h4">The Layout Library</h1>
+                <h2 class="uk-h1">Incredible Layouts</h2>
+                <p>Building a layout has never been easier. Anything you can think of can be built. Just imagine and create with YOOtheme Pro.</p>
+
+                <div class="uk-child-width-1-4@m uk-child-width-1-2@s" uk-grid>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/copper-hill">
+                            <img src="pro/layout-copper-hill.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/sonic">
+                            <img src="pro/layout-sonic.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fjord">
+                            <img src="pro/layout-fjord.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/joline">
+                            <img src="pro/layout-joline.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/max">
+                            <img src="pro/layout-max.jpg">
+                        </a>
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/horizon">
+                            <img src="pro/layout-horizon.jpg">
+                        </a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fuse">
+                            <img src="pro/layout-fuse.jpg">
+                        </a>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="uk-section uk-section-primary tm-section">
+            <div class="uk-container uk-text-center">
+
+                <h1>Get YOOtheme Pro</h1>
+                <p>dsafadsf asdfasdf asdf asdf asdf asf</p>
+
+                <div class="uk-grid-small uk-child-width-auto uk-flex-inline" uk-grid>
+                    <div>
+
+                        <a class="uk-button uk-button-default uk-button-large" href="#">Buy Now</a>
+
+                    </div>
+                    <div>
+
+                        <a class="uk-button uk-button-default uk-button-large" href="#">Browse Themes</a>
+
+                    </div>
+                </div>
+
             </div>
         </div>
 
     </div>
 
-    <div class="uk-section uk-section-primary">
-        <div class="uk-container">
-
-            <div class="uk-child-width-expand@m uk-child-width-1-2@s" uk-grid>
-                <div>
-
-                    <h3>Elements</h3>
-                    <p>Build pages with ready to use UIkit elements like buttons, cards, accordion and many more.</p>
-
-                </div>
-                <div>
-
-                    <h3>Header Layouts</h3>
-                    <p>Modify your sticky navbar to dropdown or configure where items inside the header should be displayed.</p>
-
-                </div>
-                <div>
-
-                    <h3>Page Builder</h3>
-                    <p>Create your UIkit pages in next to no time. Our innovative page builder makes it possible to arrange sections.</p>
-
-                </div>
-                <div>
-
-                    <h3>Real-Time Preview</h3>
-                    <p>Implement your ideas and see immediately how they look like. Our real-time-preview makes it</p>
-
-                </div>
-            </div>
-
-            <div class="uk-child-width-expand@m" uk-grid>
-                <div class="uk-flex-last@m">
-
-                    <h1 class="uk-h4">Style Customizer</h1>
-                    <h2 class="uk-h1">Style Customizer</h2>
-                    <p>Arrange visual appearance of your site, including colors, fonts, margins and other style related properties with a full style customizer for our UIkit CSS framework.</p>
-
-                </div>
-                <div>
-
-                    <img src="test.jpg">
-
-                </div>
-            </div>
-
-            <h2>Pre-built Styles</h2>
-
-            <div class="uk-child-width-1-3@m uk-child-width-1-2@s" uk-grid>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/copper-hill">
-                        <img src="pro/style-copper-hill.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/sonic">
-                        <img src="pro/style-sonic.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fjord">
-                        <img src="pro/style-fjord.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/joline">
-                        <img src="pro/style-joline.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/max">
-                        <img src="pro/style-max.jpg">
-                    </a>
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/horizon">
-                        <img src="pro/style-horizon.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fuse">
-                        <img src="pro/style-fuse.jpg">
-                    </a>
-
-                </div>
-            </div>
-
-            <hr>
-
-            <h1 class="uk-h4">The Layout Library</h1>
-            <h2 class="uk-h1">Incredible Layouts</h2>
-            <p>Building a layout has never been easier. Anything you can think of can be built. Just imagine and create with YOOtheme Pro.</p>
-
-            <div class="uk-child-width-1-4@m uk-child-width-1-2@s" uk-grid>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/copper-hill">
-                        <img src="pro/layout-copper-hill.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/sonic">
-                        <img src="pro/layout-sonic.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fjord">
-                        <img src="pro/layout-fjord.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/joline">
-                        <img src="pro/layout-joline.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/max">
-                        <img src="pro/layout-max.jpg">
-                    </a>
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/horizon">
-                        <img src="pro/layout-horizon.jpg">
-                    </a>
-
-                </div>
-                <div>
-
-                    <a class="uk-box-shadow-small uk-box-shadow-hover-large" href="https://demo.yootheme.com/wordpress/fuse">
-                        <img src="pro/layout-fuse.jpg">
-                    </a>
-
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    <div class="uk-section uk-section-primary tm-section">
-        <div class="uk-container uk-text-center">
-
-            <h1>Get YOOtheme Pro</h1>
-            <p>dsafadsf asdfasdf asdf asdf asdf asf</p>
-
-            <div class="uk-grid-small uk-child-width-auto uk-flex-inline" uk-grid>
-                <div>
-
-                    <a class="uk-button uk-button-default uk-button-large" href="#">Buy Now</a>
-
-                </div>
-                <div>
-
-                    <a class="uk-button uk-button-default uk-button-large" href="#">Browse Themes</a>
-
-                </div>
-            </div>
-
-        </div>
-    </div>
 
 </template>
