ccff284ab1b863aade0b589791acdea330fcc68b
Steffan <steffan@schiewe.biz>
2017-04-24 19:00:13 +0200

    update scripts

--- docs/app/main.js
+++ docs/app/main.js
@@ -1,24 +1,33 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './app.vue';
 import Page from './page.vue';
+import navigation from './navigation.json';
 
 const routes = [
-
-    {
-        path: '*/docs/',
-        redirect: '*/docs/introduction'
-    },
-
-    {
-        path: '*/docs/:page',
-        component: Page
-    }
-
+    {path: '*/docs/', redirect: '*/docs/introduction'},
+    {path: '*/docs/:page', component: Page}
 ];
 
-const navigation = require('./navigation.json');
+const router = new VueRouter({
+    routes,
+    mode: 'history',
+    history: true,
+    linkActiveClass: 'uk-active'
+});
+
+router.afterEach(({path}) => {
+
+    if (typeof ga == 'function') {
+        ga('set', 'page', path);
+        ga('send', 'pageview');
+    }
+
+});
 
 window.DocsApp = new Vue({
+
+    router,
 
     el: '#app',
 
@@ -27,15 +36,8 @@ window.DocsApp = new Vue({
     data: () => ({
         navigation,
         ids: {},
-        component: false,
-        page: false
-    }),
-
-    router: new VueRouter({
-        routes,
-        mode: 'history',
-        history: true,
-        linkActiveClass: 'uk-active'
+        page: false,
+        component: false
     })
 
 });
