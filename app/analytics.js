1166fdfeb044e539aeafeed78688d82fdf305486
janschoenherr <jan@yootheme.com>
2018-03-22 17:54:35 +0100

    update google analytics loading 🤪

--- app/analytics.js
+++ app/analytics.js
@@ -1,12 +1,20 @@
 export default function() {
 
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
+    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+    window.addEventListener('load', () => getScript('//www.google-analytics.com/analytics.js'));
 
     ga('create', 'UA-42150424-1', 'getuikit.com');
     ga('send', 'pageview');
+
+}
+
+export function getScript(src) {
+
+    const script = document.createElement('script');
+
+    script.src = src;
+    script.async = true;
+
+    document.head.appendChild(script);
 
 }
