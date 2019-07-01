12bfcfb72dd8f68566b55768a618926fb7083436
Sascha Dube <sascha.dube@gmail.com>
2019-07-01 16:18:52 +0200

    fix 404 page

--- app/pages/404.vue
+++ app/pages/404.vue
@@ -4,11 +4,11 @@
 
         <navbar class="uk-navbar-transparent"/>
 
-        <div class="uk-section uk-section-small uk-text-center" uk-height-viewport>
+        <div class="uk-section uk-section-small uk-flex uk-flex-middle uk-text-center" uk-height-viewport>
             <div class="uk-width-1-1">
                 <div class="uk-container">
 
-                    <h1 class="uk-margin-medium uk-text-center">404</h1>
+                    <h1 class="uk-font-7xlarge uk-margin-medium uk-text-center">404</h1>
 
                     <p class="uk-font-large">Page not found</p>
 
