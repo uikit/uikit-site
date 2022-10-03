7d582a95ec97996975e7551cf751db0cf19dbb3f
Sascha Dube <sascha.dube@gmail.com>
2022-10-03 12:59:31 +0200

    remove role and aria-label attributes in favor of setting them through the UIkit JS

--- app/navbar.vue
+++ app/navbar.vue
@@ -45,7 +45,6 @@
                         uk-navbar-toggle-icon
                         href="#offcanvas"
                         uk-toggle
-                        aria-label="Open Menu"
                     ></a>
                 </div>
             </nav>
