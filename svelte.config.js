1fffee8815e2b4fa79b284150424c2a2302242a7
janschoenherr <jan@yootheme.com>
2023-02-27 15:09:49 +0100

    add cache breaking

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
+import { readFile } from 'node:fs/promises';
+import crypto from 'crypto';
+
+process.env.PUBLIC_CACHE = createHash(await readFile('./package.json', 'utf-8'), 2);
 
 const config = {
     kit: {
@@ -7,3 +11,7 @@ const config = {
 };
 
 export default config;
+
+function createHash(data, length) {
+    return crypto.createHash('shake256', { outputLength: length }).update(data).digest('hex');
+}
