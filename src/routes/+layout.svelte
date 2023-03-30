39f733691c867641fd359a4420e72a21d820ef4d
janschoenherr <jan@yootheme.com>
2023-03-30 10:25:12 +0200

    exclude analytics in dev mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,25 @@
 <script>
     import Offcanvas from './Offcanvas.svelte';
-    import { base } from '$app/paths';
+    import { assets, base } from '$app/paths';
     import { page } from '$app/stores';
+
+    import { PUBLIC_CACHE } from '$env/static/public';
+    import { dev } from '$app/environment';
+
+    export let devMode = dev;
+
     export let data;
 </script>
+
+<svelte:head>
+    {#if !devMode}
+        <script
+            src="{assets}/analytics.js?{PUBLIC_CACHE}"
+            onload={`analytics('UA-42150424-1', { anonymize_ip: true })`}
+            defer
+        ></script>
+    {/if}
+</svelte:head>
 
 <slot />
 
